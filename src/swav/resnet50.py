6d4c96c2a8b9350f09d5840ff582c60bd487805b
Koen van Esterik <koen@vanesterik.dev>
2026-05-06 16:49:51 +0200

    fix: accept device in resnet class

--- src/swav/resnet50.py
+++ src/swav/resnet50.py
@@ -133,20 +133,21 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
     def __init__(
-            self,
-            block,
-            layers,
-            zero_init_residual=False,
-            groups=1,
-            widen=1,
-            width_per_group=64,
-            replace_stride_with_dilation=None,
-            norm_layer=None,
-            normalize=False,
-            output_dim=0,
-            hidden_mlp=0,
-            nmb_prototypes=0,
-            eval_mode=False,
+        self,
+        block,
+        layers,
+        zero_init_residual=False,
+        groups=1,
+        widen=1,
+        width_per_group=64,
+        replace_stride_with_dilation=None,
+        norm_layer=None,
+        normalize=False,
+        output_dim=0,
+        hidden_mlp=0,
+        nmb_prototypes=0,
+        eval_mode=False,
+        device="cuda",
     ):
         super(ResNet, self).__init__()
         if norm_layer is None:
@@ -154,6 +155,7 @@ class ResNet(nn.Module):
         self._norm_layer = norm_layer
 
         self.eval_mode = eval_mode
+        self.device = device
         self.padding = nn.ConstantPad2d(1, 0.0)
 
         self.inplanes = width_per_group * widen
@@ -181,15 +183,27 @@ class ResNet(nn.Module):
         self.layer1 = self._make_layer(block, num_out_filters, layers[0])
         num_out_filters *= 2
         self.layer2 = self._make_layer(
-            block, num_out_filters, layers[1], stride=2, dilate=replace_stride_with_dilation[0]
+            block,
+            num_out_filters,
+            layers[1],
+            stride=2,
+            dilate=replace_stride_with_dilation[0],
         )
         num_out_filters *= 2
         self.layer3 = self._make_layer(
-            block, num_out_filters, layers[2], stride=2, dilate=replace_stride_with_dilation[1]
+            block,
+            num_out_filters,
+            layers[2],
+            stride=2,
+            dilate=replace_stride_with_dilation[1],
         )
         num_out_filters *= 2
         self.layer4 = self._make_layer(
-            block, num_out_filters, layers[3], stride=2, dilate=replace_stride_with_dilation[2]
+            block,
+            num_out_filters,
+            layers[3],
+            stride=2,
+            dilate=replace_stride_with_dilation[2],
         )
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
 
@@ -200,7 +214,9 @@ class ResNet(nn.Module):
         if output_dim == 0:
             self.projection_head = None
         elif hidden_mlp == 0:
-            self.projection_head = nn.Linear(num_out_filters * block.expansion, output_dim)
+            self.projection_head = nn.Linear(
+                num_out_filters * block.expansion, output_dim
+            )
         else:
             self.projection_head = nn.Sequential(
                 nn.Linear(num_out_filters * block.expansion, hidden_mlp),
@@ -308,13 +324,18 @@ class ResNet(nn.Module):
     def forward(self, inputs):
         if not isinstance(inputs, list):
             inputs = [inputs]
-        idx_crops = torch.cumsum(torch.unique_consecutive(
-            torch.tensor([inp.shape[-1] for inp in inputs]),
-            return_counts=True,
-        )[1], 0)
+        idx_crops = torch.cumsum(
+            torch.unique_consecutive(
+                torch.tensor([inp.shape[-1] for inp in inputs]),
+                return_counts=True,
+            )[1],
+            0,
+        )
         start_idx = 0
         for end_idx in idx_crops:
-            _out = self.forward_backbone(torch.cat(inputs[start_idx: end_idx]).cuda(non_blocking=True))
+            inputs = torch.cat(inputs[start_idx:end_idx])
+            inputs = inputs.cuda(non_blocking=True) if self.device == "cuda" else inputs
+            _out = self.forward_backbone(inputs)
             if start_idx == 0:
                 output = _out
             else:
